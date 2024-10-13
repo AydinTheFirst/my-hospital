32c61971e4fd408f7b37836479896f4f50e113c8
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2024-10-13 19:36:09 +0300

    Refactor Navbar component and add new route for creating appointments

--- apps/client/src/pages/(home)/_layout.tsx
+++ apps/client/src/pages/(home)/_layout.tsx
@@ -1,8 +1,21 @@
+import {
+  Button,
+  Link,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Navbar as NextNavbar,
+} from "@nextui-org/react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
     <>
+      <Navbar />
       <main className="container px-3 py-16 md:px-10">
         <div className="grid gap-5">
           <Outlet />
@@ -13,3 +26,65 @@ const HomeLayout = () => {
 };
 
 export default HomeLayout;
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    {
+      href: "/randevular",
+      title: "Randevular",
+    },
+    {
+      href: "/randevular/yeni",
+      title: "Yeni Randevu",
+    },
+    {
+      href: "/dashboard",
+      title: "Yönetim Paneli",
+    },
+  ];
+
+  return (
+    <NextNavbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={index}>
+            <Link color="foreground" href={item.href} size="lg">
+              {item.title}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/login">Giriş yap</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/register" variant="flat">
+            Kayıt Ol
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="w-full" href={item.href} size="lg">
+              {item.title}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </NextNavbar>
+  );
+};
